b015981626f0d1299c186408617a5f418fda992d
NeilRow56 <neilrowland56@gmail.com>
2024-06-03 16:36:05 +0100

    Added convex files.ts and schema.ts Basic useage of database now - with aur=thentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  useSession,
+} from '@clerk/nextjs'
+import { useMutation, useQuery } from 'convex/react'
+
 export default function Home() {
+  const createFile = useMutation(api.files.createFile)
+  const files = useQuery(api.files.getFiles)
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
+      <SignedIn>
+        <SignOutButton>
+          <Button className="mb-10">Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button className="mb-10">Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+
       <h1 className="text-4xl font-semibold text-primary">Doctor app</h1>
+      <Button
+        className="m-10"
+        onClick={() => {
+          createFile({ name: 'Hello World!' })
+        }}
+      >
+        Click Me
+      </Button>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>
+      })}
     </main>
   )
 }
